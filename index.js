3022f1744629e1e11445eb54943cf7ef4ee541a8
Creatorsbbs <serisvaldo927@gmail.com>
2026-04-18 20:15:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
-});
+  intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+]
 
 client.once("ready", () => {
   console.log(`Logado como ${client.user.tag}`);
