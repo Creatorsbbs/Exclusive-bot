c579fe7542bf21f3b16c9c5765a0518a0a47bf90
Creatorsbbs <serisvaldo927@gmail.com>
2026-05-03 11:13:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+require("dotenv").config();
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
+
+// 🔥 puxando sistema de música
+require("./music")(client);
 
 client.once("ready", () => {
-  console.log(`Logado como ${client.user.tag}`);
+  console.log(`🤖 Online como ${client.user.tag}`);
 });
-
-require("./mod")(client);
 
 client.login(process.env.TOKEN);
