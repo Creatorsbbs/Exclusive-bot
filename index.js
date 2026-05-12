feb94a6a9f290c6a02b3ca5c41b5d9968449d124
Creatorsbbs <serisvaldo927@gmail.com>
2026-05-12 09:36:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -41,17 +41,32 @@ async function setupServer(guild) {
 
     // ================= CARGO STAFF =================
     let staffRoleId = await db.get(`staffRole_${guild.id}`);
-let staffRole = staffRoleId ? guild.roles.cache.get(staffRoleId) : null;
 
-    if (!staffRole) {
-  staffRole = await guild.roles.create({
-    name: "STAFF",
-    color: "Red"
-  });
+let staffRole = null;
+
+if (staffRoleId) {
+  staffRole = guild.roles.cache.get(staffRoleId);
+
+  if (!staffRole) {
+    staffRole = await guild.roles.fetch(staffRoleId).catch(() => null);
+  }
+}
+
+if (!staffRole) {
+  staffRole = guild.roles.cache.find(r => r.name === "STAFF");
+
+  if (!staffRole) {
+    staffRole = await guild.roles.create({
+      name: "STAFF",
+      color: "Red"
+    });
+
+    console.log("✔ Cargo STAFF criado");
+  }
 
   await db.set(`staffRole_${guild.id}`, staffRole.id);
-  console.log("✔ Cargo STAFF criado");
 }
+  
 
     // ================= CANAL LOGS =================
     let logChannel = guild.channels.cache.find(c => c.name === "logs");
