a43a0404dff8b6e4108a01770ab7f25b7d5ff324
Creatorsbbs <serisvaldo927@gmail.com>
2026-05-15 16:45:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ Propostas de parceria, divulgação ou colaboração entre servidores.
 Explique sua situação com o máximo de detalhes possível para agilizar o atendimento.
 `)
         .setImage("https://cdn.discordapp.com/attachments/1264564541979627604/1504187640524701726/file_000000005270720e895d4916721bd3ce.png?ex=6a08b667&is=6a0764e7&hm=9ffdfd4d3da6d142e8cfaacb51b9088c34025d4947ccadb67a3df9c7b49f7240&")
-        .setColor("#2b2d31");
+        .setColor("#00b0f4");
 
       const row = new ActionRowBuilder().addComponents(
 
@@ -330,7 +330,7 @@ Seu ticket foi criado com sucesso e nossa equipe já foi notificada.
 ✨ Equipe de Suporte
 `)
         .setImage("https://cdn.discordapp.com/attachments/1264564541979627604/1504187640524701726/file_000000005270720e895d4916721bd3ce.png?ex=6a08b667&is=6a0764e7&hm=9ffdfd4d3da6d142e8cfaacb51b9088c34025d4947ccadb67a3df9c7b49f7240&")
-        .setColor("Green");
+        .setColor("#00b0f4");
 
       const row = new ActionRowBuilder().addComponents(
 
